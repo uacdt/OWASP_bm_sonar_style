3d78f004304b7a67db89ee1d13fab23310a96011
agigleux <gigleux_alexandre@yahoo.fr>
2018-12-10 18:58:24 +0100

    Organize 'testcode' by Category and Expected/NotExpected issue

---
+++ src/main/java/org/owasp/benchmark/testcode/OrganizeFiles.java
@@ -0,0 +1,56 @@
+package org.owasp.benchmark.testcode;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+public class OrganizeFiles {
+  public static void main(String[] args) {
+    InputStream stream = OrganizeFiles.class.getClassLoader().getResourceAsStream("expectedresults-1.2.csv");
+    Scanner scanner = new Scanner(stream);
+    while (scanner.hasNext()) {
+      String line = scanner.nextLine();
+      if (!line.startsWith("#")) {
+        String[] data = line.split(",");
+
+        String fileName = data[0];
+        String category = data[1];
+
+        createCategoryDirectories(category);
+
+        boolean issueExpected = "true".equals(data[2]);
+
+        try {
+          System.out.println(fileName);
+          if (issueExpected) {
+            Files.move(new File("src/main/java/org/owasp/benchmark/testcode/", fileName + ".java").toPath(),
+              new File("src/main/java/org/owasp/benchmark/testcode/" + category + "/compliant/", fileName + ".java").toPath(), REPLACE_EXISTING);
+          } else {
+            Files.move(new File("src/main/java/org/owasp/benchmark/testcode/", fileName + ".java").toPath(),
+              new File("src/main/java/org/owasp/benchmark/testcode/" + category + "/noncompliant/", fileName + ".java").toPath(), REPLACE_EXISTING);
+          }
+        } catch (IOException e) {
+          System.out.println(e.getMessage());
+        }
+
+      }
+    }
+    scanner.close();
+  }
+
+  private static void createCategoryDirectories(String category) {
+    File compliantCategoryDir = new File("src/main/java/org/owasp/benchmark/testcode/" + category + "/compliant/");
+    if (!compliantCategoryDir.exists()) {
+      compliantCategoryDir.mkdirs();
+    }
+
+    File noncompliantCategoryDir = new File("src/main/java/org/owasp/benchmark/testcode/" + category + "/noncompliant/");
+    if (!noncompliantCategoryDir.exists()) {
+      noncompliantCategoryDir.mkdirs();
+    }
+  }
+}
